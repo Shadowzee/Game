f422173be9ad255d58dca33ea274beeede7d00ff
Nishant Pratap <singhnishant411@gmail.com>
2019-12-17 21:37:20 +0530

    Questions are getting from fake rest api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgxGaugeModule } from 'ngx-gauge';
-
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
-
 import { MyPipe } from './mypipe';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -17,7 +17,8 @@ import { MyPipe } from './mypipe';
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    NgxGaugeModule
+    NgxGaugeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ConfigService } from '../config.service';
 
 
 @Component({
@@ -13,7 +14,8 @@ score=0;
 questions;
 question=[];
 form:FormGroup;
-
+url="http://localhost:3000/questions";
+submiturl="http://localhost:3000/submissions";
 gaugeType = "semi";
 gaugeValue = this.score;
 max=10;
@@ -32,29 +34,20 @@ thresholds={
 }
 gaugeLabel = "Score";
 gaugeAppendText = "/ 10";
- ans=["thi",'sec']
-  constructor(private fb: FormBuilder) {
-    this.form = this.fb.group({
-      question: ['']
-      
+ ques;
+
+  constructor(private fb: FormBuilder,private config:ConfigService) {
+    config.getquestions(this.url).subscribe(res=>{
+      this.ques=res;
+      this.form = this.fb.group({
+        question: ['']
+        
+      });
+      this.question=this.ques;
     });
-    this.question=this.getques();
+   
    }
-   getques(){
-    return [
-      { id: '1', name: 'Q 1',ques:"A farmer has 17 sheep and all but nine die. How many are left?",options:[{val:"Eight"},{val:"Seventeen"},{val:"Zero"},{val:"Nine"}],ans:"Nine" },
-      { id: '2', name: 'Q 2',ques:"Who is bigger: Mr. Bigger, Mrs. Bigger, or their baby?" ,options:[{val:"Mr.Bigger"},{val:"Mrs. Bigger"},{val:"Baby"},{val:"You"}],ans:"Baby" },
-      { id: '3', name: 'Q 3' ,ques:"What are the next three letters in the following sequence?J, F, M, A, M, J, J, A, __, __, __?",options:[{val:"S,O,N"},{val:"D,H,M"},{val:"M,Q,Q"},{val:"R,Z,A"}],ans:"S,O,N" },
-      { id: '4', name: 'Q 4' ,ques:"Jimmy’s mother had four children. She named the first Monday. She named the second Tuesday, and she named the third Wednesday. What is the name of the fourth child?",options:[{val:"Thrushday"},{val:"Friday"},{val:"Janny"},{val:"Jimmy"}],ans:"Jimmy" },
-      { id: '5', name: 'Q 5',ques:"Which travels faster?",options:[{val:"Hot"},{val:"Cold"},{val:"Feelings"},{val:"Ball"}],ans:"Hot" },
-      { id: '6', name: 'Q 6',ques:"8 5 4 9 1 7 6 10 ,_,_? " ,options:[{val:"3,2"},{val:"11,1"},{val:"9,8"},{val:"3,8"}],ans:"3,2" },
-      { id: '7', name: 'Q 7' ,ques:"What day would yesterday be if Thursday was four days before the day after tommorow?",options:[{val:"Monday"},{val:"Thrusday"},{val:"Friday"},{val:"Saturday"}],ans:"Friday" },
-      { id: '8', name: 'Q 8' ,ques:"The following sentence is false. The preceding sentence is true. Are these sentences true or false?",options:[{val:"True"},{val:"False"},{val:"Neither"},{val:"Both"}],ans:"Neither" },
-      { id: '9', name: 'Q 9',ques:"A queen bee was buzzing, a worker bee was buzzing, a honey bee was buzzing, and a killer bee was buzzing. How many bees were in buzzing?",options:[{val:"Four"},{val:"One"},{val:"Zero"},{val:"Two"}],ans:"One" },
-      { id: '10', name: 'Q 10',ques:"A dog had three puppies, named Mopsy, Topsy and Spot. What was the mothers name." ,options:[{val:"Mrs. Mopsy"},{val:"Mopsy"},{val:"What"},{val:"Why"}],ans:"What" },
-      
-    ];
-   }
+   
 
   ngOnInit() {
   }
@@ -66,12 +59,16 @@ console.log(this.form.value)
 if(this.form.controls.question.value==this.question[this.count-1].ans){
   this.score++;
 }
-
+this.submitscore();
 
 this.count++;
 this.gaugeValue = this.score;
 this.form.reset();
 }
-
+submitscore(){
+  this.config.createscore(this.submiturl,{hi:"hi"}).subscribe(res=>{
+    console.log(res);
+  })
+}
 
 }
